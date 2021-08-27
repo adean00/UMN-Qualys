b69534522e6d04a8dad2314b9703c5ac84567817
Aaron Dean <adean@dgaplans.org>
2021-08-27 16:22:02 -0700

    WIP: Get-QualysReport testing for usage ability

--- module test.ps1
+++ module test.ps1
@@ -75,6 +75,51 @@ function Connect-Qualys{
 }
 #endregion
 
+#region Disconnect-Qualys
+function Disconnect-Qualys{
+    <#
+        .Synopsis
+           Disconnect Qaulys API Session, this only works on the old API
+    
+        .DESCRIPTION
+             Disconnect Qaulys API Session, this only works on the old API
+    
+        .PARAMETER qualysServer
+                FQDN of qualys server, see Qualys documentation, based on wich Qualys Platform you're in.
+    
+        .PARAMETER cookie
+            Use Connect-Qualys to get session cookie
+    
+        .EXAMPLE
+            disconnect-Qualys -uri 'https://qualysapi.qualys.com:443/api/2.0/fo/session/' -header (Get-QualysHeader) 
+            
+        .Notes
+            Author: Travis Sobeck
+    #>
+        [CmdletBinding()]
+        Param
+        (
+            [Parameter(Mandatory)]
+            [string]$qualysServer,
+    
+            [Parameter(Mandatory)]
+            [Microsoft.PowerShell.Commands.WebRequestSession]$cookie
+        )
+    
+        Begin{}
+        Process
+        {
+            ## Login/out
+            $logInBody = @{action = "logout"}
+            $return = (Invoke-RestMethod -Headers @{"X-Requested-With"="powershell"} -Uri "https://$qualysServer/api/2.0/fo/session/" -Method Post -Body $logInBody -WebSession $cookie).SIMPLE_RETURN.RESPONSE.TEXT
+            if ($return -eq 'Logged out'){return $true}
+            else{Write-Warning "Qualys logout issue" + $return}
+        }
+        End{}
+    }
+    #endregion
+    
+
 #region Get-QualysReportList
 function Get-QualysReportList{
     <#
@@ -222,13 +267,136 @@ function Get-QualysScanList{
 }
 #endregion
 
+#region Get-QualysReport
+function Get-QualysReport{
+    <#
+        .Synopsis
+            Download Qualys Report
+    
+        .DESCRIPTION
+            Download Qualys Report
+    
+        .PARAMETER id
+            Report ID, use Get-QualysReportList to find the ID
+    
+        .PARAMETER qualysServer
+                FQDN of qualys server, see Qualys documentation, based on wich Qualys Platform you're in.
+    
+        .PARAMETER cookie
+            Use Connect-Qualys to get session cookie
+    
+        .EXAMPLE
+            
+    
+        .EXAMPLE
+            
+    #>
+    
+        [CmdletBinding()]
+        Param
+        (
+            [Parameter(Mandatory)]
+            [string]$id,
+            
+            [Parameter(Mandatory)]
+            [string]$outFilePath,
+    
+            [Parameter(Mandatory)]
+            [string]$qualysServer,
+    
+            [Parameter(Mandatory)]
+            [Microsoft.PowerShell.Commands.WebRequestSession]$cookie
+        )
+    
+        Begin{}
+        Process
+        {
+            ### get the format type
+            $format = (Get-QualysReportList -qualysServer $qualysServer -cookie $cookie -id $id).OUTPUT_FORMAT
+            $outfile = "$outFilePath\qualysReport$ID.$format"
+            ## Create URL, see API docs for path  
+            $null = Invoke-RestMethod -Headers @{"X-Requested-With"="powershell"} -Uri "https://$qualysServer/api/2.0/fo/report/" -Method get -Body @{action = "fetch";id = "$id"} -WebSession $cookie -OutFile $outfile
+        }
+        End{return $outfile}
+    }
+    #endregion
+    
+#region Get-QualysScanResults
+function Get-QualysScanResults{
+    <#
+        .Synopsis
+            Get results of Qualys Scan
+
+        .DESCRIPTION
+            Get reults of Qualys Scan
+            
+        .PARAMETER scanRef
+            Qualys Scan Reference, use Get-QualysScanList to find the reference
+
+        .PARAMETER additionalOptions
+            See documentation for full list of additional options and pass in as hashtable
+
+        .PARAMETER summary
+            Use this switch to get just the title and Ref for faster searching
+        
+        .PARAMETER qualysServer
+            FQDN of qualys server, see Qualys documentation, based on wich Qualys Platform you're in.
+
+        .PARAMETER cookie
+            Use Connect-Qualys to get session cookie
+
+        .EXAMPLE
+            
+
+        .EXAMPLE
+            
+    #>
+
+    [CmdletBinding()]
+    Param
+    (
+        [string]$scanRef,
+
+        [System.Collections.Hashtable]$additionalOptions,
+
+        [switch]$brief,
+
+        [Parameter(Mandatory)]
+        [string]$qualysServer,
+
+        [Parameter(Mandatory)]
+        [Microsoft.PowerShell.Commands.WebRequestSession]$cookie
+    )
+
+    Begin{}
+    Process
+    {
+        ## Create URL, see API docs for path
+        #########################
+        $actionBody = @{action = "fetch";scan_ref = $scanRef;output_format='json'}
+        if($additionalOptions){$actionBody += $additionalOptions}
+        if($brief){$actionBody += @{mode='brief'}}
+        Invoke-RestMethod -Headers @{"X-Requested-With"="powershell"} -Uri "https://$qualysServer/api/2.0/fo/scan/" -Method Get -Body $actionBody -WebSession $cookie
+        
+    }
+    End{}
+}
+#endregion
 
 
 
 
 
-#$qualysCred = Get-Credential
+<# run once do not run on each test
+$qualysCred = Get-Credential
 $qualysServer = "qualysguard.qg3.apps.qualys.com"
 $cookie = Connect-Qualys -qualysCred $qualysCred -qualysServer $qualysServer
-#$rlist=Get-QualysReportList -qualysServer $qualysServer -cookie $cookie
+#>
+
+$rlist=Get-QualysReportList -qualysServer $qualysServer -cookie $cookie
 $slist=Get-QualysScanList -qualysServer $qualysServer -cookie $cookie
+#Get-QualysReport -qualysServer $qualysServer -cookie $cookie -outFilePath c:\temp\ -id
+
+<# disconnect after testing
+Disconnect-Qualys -qualysServer $qualysServer -cookie $cookie
+#>
